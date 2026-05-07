d2dae8ea0b7e874a1f88d5242b78decbbafaff53
Hillel Arnold <hillel.arnold@gmail.com>
2026-05-07 14:54:04 -0400

    more logging

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -304,6 +304,7 @@ class DataFetcher:
         """Sends an error message to an SNS topic."""
         client = self.get_client_with_role('sns', self.sns_role_arn)
         tb = ''.join(traceback.format_exception(exception)[:-1])
+        logging.info(tb)
         client.publish(
             TopicArn=self.sns_topic,
             MessageGroupId=f'{self.service_name}-{self.object_type}-{self.object_status}',
